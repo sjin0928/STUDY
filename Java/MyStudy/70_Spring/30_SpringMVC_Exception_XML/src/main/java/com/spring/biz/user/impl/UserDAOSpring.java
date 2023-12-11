e50c47d8e4e42c4daccc215b4f24facb7b51c5f9
sjin0928 <143140370+sjin0928@users.noreply.github.com>
2023-12-11 15:53:50 +0900

    Add files via upload

---
+++ Java/MyStudy/70_Spring/30_SpringMVC_Exception_XML/src/main/java/com/spring/biz/user/impl/UserDAOSpring.java
@@ -0,0 +1,28 @@
+package com.spring.biz.user.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.spring.biz.user.UserVO;
+
+@Repository
+public class UserDAOSpring {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	//SQL문
+	private final String USER_GET
+		= "SELECT * FROM USERS WHERE ID = ? AND PASSWORD = ?";
+	
+	public UserDAOSpring() {
+		System.out.println(">> UserDAOSpring() 객체 생성");
+	}
+	
+	public UserVO getUser(UserVO vo) {
+		System.out.println(">> Spring JDBC로 getUser() 실행");
+		Object[] args = { vo.getId(), vo.getPassword() };
+		return jdbcTemplate.queryForObject(USER_GET, args, new UserRowMapper());
+	}
+	
+}
