8cbedb122ab66d68be13e5b728ea8f3793e47b2e
sjin0928 <143140370+sjin0928@users.noreply.github.com>
2023-12-11 14:54:05 +0900

    Add files via upload

---
+++ Java/MyStudy/70_Spring/11_AOP_Advice_JoinPoint/src/main/java/com/spring/biz/common/AfterThrowingAdvice.java
@@ -0,0 +1,23 @@
+package com.spring.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+
+public class AfterThrowingAdvice {
+//	public void exceptionLog() {
+//		System.out.println("[후처리-AfterThrowingAdvice.exceptionLog]"
+//				+ "비즈니스 로직 수행중 예외발생시 로그");
+//	}
+	
+	// exceptObj : 스프링 설정 파일에 설정 추가
+	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
+		
+		System.out.println("[후처리-예외발생] 예외메시지 - " + exceptObj.getMessage());
+		if(exceptObj instanceof IllegalArgumentException) {
+			System.out.println(">>> 부적합한 값이 전달되었습니다.");
+		} else if(exceptObj instanceof NumberFormatException) {
+			System.out.println(">>> 숫자형식이 아닙니다.");
+		} else if(exceptObj instanceof Exception) {
+			System.out.println(">>> 예외가 발생했습니다.");
+		}
+	}
+}
